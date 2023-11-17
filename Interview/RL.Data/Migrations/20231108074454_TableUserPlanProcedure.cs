0a4ef857eb16f638b03a8c9eefc8627dded2f811
Gokul-V5 <149957447+Gokul-V5@users.noreply.github.com>
2023-11-17 14:29:22 +0530

    Add files via upload

---
+++ Interview/RL.Data/Migrations/20231108074454_TableUserPlanProcedure.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RL.Data.Migrations
+{
+    public partial class TableUserPlanProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserPlanProcedures",
+                columns: table => new
+                {
+                    ProcedureId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PlanId = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPlanProcedures", x => new { x.PlanId, x.ProcedureId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserPlanProcedures_Plans_PlanId",
+                        column: x => x.PlanId,
+                        principalTable: "Plans",
+                        principalColumn: "PlanId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserPlanProcedures_Procedures_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "Procedures",
+                        principalColumn: "ProcedureId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserPlanProcedures_ProcedureId",
+                table: "UserPlanProcedures",
+                column: "ProcedureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserPlanProcedures");
+        }
+    }
+}
